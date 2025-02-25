8481eb0ac26d343f0a478e21828dfb24139adc2c
VitaliiSakhno <vitalik.tarasovich@gmail.com>
2025-02-25 16:57:20 +0100

    corrections in structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Loader from "./components/Loader/Loader";
 
-const HomePage = lazy(() => import("./pages/HomePage"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
-const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage/MovieDetailsPage"));
 const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
 const MovieReviews = lazy(() => import("./components/MovieReviews/MovieReviews"));
+const MovieList = lazy(() => import("./components/MovieList/MovieList"))
 
 const App = () => {
   return (
